22014de667304aa02ff2d97a0adf35af2c3a5304
Daniel Reintanz <85614916+FreelancerAMP@users.noreply.github.com>
2022-05-31 13:58:51 +0200

    CHANGES: FIXED child of reference to the correct id UPDATED frontend html to display nothing if array is empty

--- rpi-sources-block.php
+++ rpi-sources-block.php
@@ -17,16 +17,16 @@ class RpiSourcesBlock
         add_action('init', array($this, 'create_source_block'));
     }
 
-    public function create_source_block(){
-        if ( function_exists( 'lazyblocks' ) ) :
+    public function create_source_block()
+    {
+        if (function_exists('lazyblocks')) :
 
             $block_id = random_int(10, 99999999);
-            lazyblocks()->add_block( array(
+            lazyblocks()->add_block(array(
                 'id' => $block_id,
                 'title' => 'Quellennachweise',
                 'icon' => '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10.08 10.86c.05-.33.16-.62.3-.87s.34-.46.59-.62c.24-.15.54-.22.91-.23.23.01.44.05.63.13.2.09.38.21.52.36s.25.33.34.53.13.42.14.64h1.79c-.02-.47-.11-.9-.28-1.29s-.4-.73-.7-1.01-.66-.5-1.08-.66-.88-.23-1.39-.23c-.65 0-1.22.11-1.7.34s-.88.53-1.2.92-.56.84-.71 1.36S8 11.29 8 11.87v.27c0 .58.08 1.12.23 1.64s.39.97.71 1.35.72.69 1.2.91 1.05.34 1.7.34c.47 0 .91-.08 1.32-.23s.77-.36 1.08-.63.56-.58.74-.94.29-.74.3-1.15h-1.79c-.01.21-.06.4-.15.58s-.21.33-.36.46-.32.23-.52.3c-.19.07-.39.09-.6.1-.36-.01-.66-.08-.89-.23-.25-.16-.45-.37-.59-.62s-.25-.55-.3-.88-.08-.67-.08-1v-.27c0-.35.03-.68.08-1.01zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" /></svg>',
-                'keywords' => array(
-                ),
+                'keywords' => array(),
                 'slug' => 'lazyblock/reli-quellennachweis',
                 'description' => '',
                 'category' => 'text',
@@ -52,7 +52,7 @@ class RpiSourcesBlock
                     ),
                 ),
                 'controls' => array(
-                    'control_'.$block_id.'a' => array(
+                    'control_' . $block_id . 'a' => array(
                         'type' => 'text',
                         'name' => 'quellennachweise',
                         'default' => 'Quellennachweise',
@@ -68,7 +68,7 @@ class RpiSourcesBlock
                         'placeholder' => '',
                         'characters_limit' => '',
                     ),
-                    'control_'.$block_id.'b' => array(
+                    'control_' . $block_id . 'b' => array(
                         'type' => 'repeater',
                         'name' => 'quelle',
                         'default' => '',
@@ -90,13 +90,13 @@ class RpiSourcesBlock
                         'placeholder' => '',
                         'characters_limit' => '',
                     ),
-                    'control_'.$block_id.'c' => array(
+                    'control_' . $block_id . 'c' => array(
                         'type' => 'text',
                         'name' => 'title',
                         'default' => '',
                         'label' => '',
                         'help' => '',
-                        'child_of' => 'control_e1492843e1',
+                        'child_of' => 'control_' . $block_id . 'b',
                         'placement' => 'content',
                         'width' => '100',
                         'hide_if_not_selected' => 'false',
@@ -106,13 +106,13 @@ class RpiSourcesBlock
                         'placeholder' => 'Titel der Quelle',
                         'characters_limit' => '',
                     ),
-                    'control_'.$block_id.'d' => array(
+                    'control_' . $block_id . 'd' => array(
                         'type' => 'text',
                         'name' => 'src',
                         'default' => '',
                         'label' => '',
                         'help' => '',
-                        'child_of' => 'control_e1492843e1',
+                        'child_of' => 'control_' . $block_id . 'b',
                         'placement' => 'content',
                         'width' => '100',
                         'hide_if_not_selected' => 'false',
@@ -122,13 +122,13 @@ class RpiSourcesBlock
                         'placeholder' => 'Url',
                         'characters_limit' => '',
                     ),
-                    'control_'.$block_id.'e' => array(
+                    'control_' . $block_id . 'e' => array(
                         'type' => 'select',
                         'name' => 'license',
                         'default' => '',
                         'label' => '',
                         'help' => '',
-                        'child_of' => 'control_e1492843e1',
+                        'child_of' => 'control_' . $block_id . 'b',
                         'placement' => 'content',
                         'width' => '100',
                         'hide_if_not_selected' => 'false',
@@ -154,13 +154,14 @@ class RpiSourcesBlock
                     'show_preview' => 'always',
                     'single_output' => false,
                 ),
-                'condition' => array(
-                ),
-            ) );
+                'condition' => array(),
+            ));
 
         endif;
     }
-    public function get_lincence_choices(){
+
+    public function get_lincence_choices()
+    {
         return array(
             array(
                 'label' => 'Lizenz wählen',
@@ -204,26 +205,37 @@ class RpiSourcesBlock
             ),
         );
     }
-    public function get_frontend_html(){
+
+    public function get_frontend_html()
+    {
         return '<?php 
     $block = lazyblocks()->blocks()->get_block( $attributes[\'lazyblock\'][\'slug\'] ); 
     $icon = $block[\'icon\'];
-    
-    ?>
-    <div>
-    <h2><?php echo $icon;?> Bildnachweise</h2>
-    <?php
-    echo \'<ul>\';
-    foreach( $attributes[\'quelle\'] as $quelle )
+    if (!empty(reset($attributes[\'quelle\'])))
     {
-        echo \'<li>\';
-        echo \'<a href ="\'.$quelle[\'src\'] .\'">\'. $quelle[\'title\'] .\'</a>\';
-        echo \' (\'.$quelle[\'license\'].\')\';
-        echo \'</li>\';
+        ?>
+        <div>
+        <h2><?php echo $icon;?> Bildnachweise</h2>
+        <?php
+        echo \'<ul>\';
+        foreach( $attributes[\'quelle\'] as $quelle )
+        {
+            if (!empty($quelle))
+            {
+                echo \'<li>\';
+                echo \'<a href ="\'.$quelle[\'src\'] .\'">\'. $quelle[\'title\'] .\'</a>\';
+                echo \' (\'.$quelle[\'license\'].\')\';
+                echo \'</li>\';
+            }
+        }
+        echo \'</ul>\';
+        ?>
+        </div>
+        <?php
     }
-    echo \'</ul>\';
     ?>
-    </div>';
+    ';
     }
 }
+
 new RpiSourcesBlock();
